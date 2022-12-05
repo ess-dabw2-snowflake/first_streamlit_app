851a7661216fb394069d24e4d6f40d533642a284
Vkumari <119501592+ess-dabw2-snowflake@users.noreply.github.com>
2022-12-05 15:57:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 streamlit.text(fruityvice_response)
 
 # New section to display fruityvice api response
-streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
+# streamlit.text(fruityvice_response.json())
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
+# lets comment out line37 and modify line 32
 
 
 
